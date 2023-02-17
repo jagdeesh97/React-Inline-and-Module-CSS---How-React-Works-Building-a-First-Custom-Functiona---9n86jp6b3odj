6ec94cf1a2d50caca876316cdba4426882555f9a
Jagdeesh Kumar <121223763+jagdeesh97@users.noreply.github.com>
2023-02-17 10:19:28 +0530

    React Inline and Module CSS

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import '../styles/App.css';
 import styles from '../styles/appStyles.module.css';
 const App = () => {
 const heading ={
-      color: 'red',
-      fontSize: 50px,
-}
+    color:'red',
+    fontSize:50
+       
+
+};
   return (
     <div id="main">
-      <h1 className="styles.success">Success</h1>
+      <h1 className={styles.success}>Success</h1>
       <h1 id='error' style={heading}>Error</h1>
     </div>
   )
